6fec5601f502cc449df5dd2b14172e5d66517f40
Ben <9087625+benfdking@users.noreply.github.com>
2024-01-21 20:41:51 -0800

    Bump to version 0.67 and add aarch architecture

--- quary.rb
+++ quary.rb
@@ -1,9 +1,17 @@
 class Quary < Formula
-  desc "Collaborative Business Intelligence.Your team's browser-based data workbench"
+  desc "Collaborative Business Intelligence. Your team's browser-based data workbench."
   homepage "https://www.quary.dev"
-  url "https://github.com/quarylabs/quary-cli/releases/download/0.0.65/quary-0.0.65-quary-macos-x86_64-gnu.zip"
-  sha256 "b56146edcb1e85723256460486b512e94d22100b36317e4aa7d1f93a4f6c2bb2"
+
+  if Hardware::CPU.intel?
+    url "https://github.com/quarylabs/quary-cli/releases/download/0.0.67/quary-0.0.67-quary-macos-x86_64-gnu.zip"
+    sha256 "8a039a44d6e276567f0c1c19341e5a52f4147b83c45861d5ea10c429b0d66658"
+    
+  elsif Hardware::CPU.arm?
+    url "https://github.com/quarylabs/quary-cli/releases/download/0.0.67/quary-0.0.67-quary-macos-aarch64-gnu.zip"
+    sha256 "c7be8ef7912bfbef0fa745f1e3b95831f04df2aee563a3a1dea69b8fed566ea0"
+  end
+
   def install
-    bin.install "quary-0.0.65" => "quary"
+    bin.install "quary-0.0.67" => "quary"
   end
 end
