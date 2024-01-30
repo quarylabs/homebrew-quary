931742a5ae77f4f2fc11b44938c01bb46a0406e5
Louis J <132601011+louisjoecodes@users.noreply.github.com>
2024-01-30 09:27:52 -0800

    feat: bump to 0.0.75

--- quary.rb
+++ quary.rb
@@ -6,13 +6,13 @@ class Quary < Formula
   desc "Collaborative Business Intelligence, your team's browser-based data workbench"
   homepage "https://www.quary.dev"
   if Hardware::CPU.intel?
-    url "https://github.com/quarylabs/quary-cli/releases/download/0.0.74/quary-0.0.74-quary-macos-x86_64-gnu.zip"
-    sha256 "2a336e6c7807414a5ac82766d169646d3ebb51d672feb9909af4c74067a1adf6"
+    url "https://github.com/quarylabs/quary-cli/releases/download/0.0.75/quary-0.0.75-quary-macos-x86_64-gnu.zip"
+    sha256 "e1e22f4382ce60f4ad4b1a7e8629e0587b86de4719c174c724d84aa44b364380"
   elsif Hardware::CPU.arm?
-    url "https://github.com/quarylabs/quary-cli/releases/download/0.0.74/quary-0.0.74-quary-macos-aarch64-gnu.zip"
-    sha256 "7969916c6827453d64515f2db71c1f2e8ed212c9817bbe9fa973239ba378c1f8"
+    url "https://github.com/quarylabs/quary-cli/releases/download/0.0.75/quary-0.0.75-quary-macos-aarch64-gnu.zip"
+    sha256 "baa3701f28855b0b426849fd7e570d45be5a4136c43f552e972d107b8af695f8"
   end
   def install
-    bin.install "quary-0.0.74" => "quary"
+    bin.install "quary-0.0.75" => "quary"
   end
 end
