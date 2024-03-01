5d1a2376f90c00bc3a2e1423750046dc0e96586b
Ben King <9087625+benfdking@users.noreply.github.com>
2024-03-01 11:22:34 -0800

    feat: adding ability to publish sqruff

--- quary.rb
+++ quary.rb
@@ -5,6 +5,7 @@
 class Quary < Formula
   desc "Collaborative Business Intelligence, your team's browser-based data workbench"
   homepage "https://www.quary.dev"
+  depends_on :macos
   if Hardware::CPU.intel?
     url "https://github.com/quarylabs/quary-cli/releases/download/0.0.93/quary-0.0.93-quary-macos-x86_64-gnu.zip"
     sha256 "0cc53cc3fc899c439afe68bf5776e7bbc71f45a56aaca4f8eaa4aef141d86166"
