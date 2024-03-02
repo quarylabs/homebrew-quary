6362cca3c8f68f44dd52cf29201a77db0f4b52f5
Ben King <9087625+benfdking@users.noreply.github.com>
2024-03-02 10:29:03 -0800

    Update quary and add linux

--- quary.rb
+++ quary.rb
@@ -5,14 +5,19 @@
 class Quary < Formula
   desc "Collaborative Business Intelligence, your team's browser-based data workbench"
   homepage "https://www.quary.dev"
-  depends_on :macos
-  if Hardware::CPU.intel?
-    url "https://github.com/quarylabs/quary/releases/download/0.0.95/quary-0.0.95-quary-macos-x86_64-gnu.zip"
-    sha256 "840273b2f8e5747e6032fa794462739775631cf96920d21ea532086f971863e6"
-  elsif Hardware::CPU.arm?
-    url "https://github.com/quarylabs/quary/releases/download/0.0.95/quary-0.0.95-quary-macos-aarch64-gnu.zip"
-    sha256 "7e174ad80c4ac61ac045d88d42d6e65dceb136238075e23177a267be99e5a6bc"
+
+  # Determine the OS and architecture to set the appropriate URL and SHA-256
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/quarylabs/quary/releases/download/0.0.96/quary-0.0.96-quary-macos-x86_64-gnu.zip"
+    sha256 "a76f55a0d8c4b987231148e2111921f7e0295b45046d4cf740d3424039055705"
+  elsif OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/quarylabs/quary/releases/download/0.0.96/quary-0.0.96-quary-macos-aarch64-gnu.zip"
+    sha256 "5694f70b4f6d5dc40df67a0af98a2530cffdade78dc2dd5620c8dd7ab93e5772"
+  elsif OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/quarylabs/quary/releases/download/0.0.96/quary-0.0.96-quary-linux-x86_64-gnu.zip"
+    sha256 "7a44a6640707c8086acda54938c93a6c6ebac6c8d275b0154a30afe5584b4f87"
   end
+
   def install
     bin.install "quary" => "quary"
   end
