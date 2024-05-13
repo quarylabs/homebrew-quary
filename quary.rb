79dcc65f410915da8d2c6c5b753658b35555bec0
Louis J <132601011+louisjoecodes@users.noreply.github.com>
2024-05-13 15:58:14 +0100

    chore: upgrade to 126

--- quary.rb
+++ quary.rb
@@ -8,14 +8,14 @@ class Quary < Formula
 
   # Determine the OS and architecture to set the appropriate URL and SHA-256
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/quarylabs/quary/releases/download/0.0.116/quary-0.0.116-quary-macos-x86_64-gnu.zip"
-    sha256 "c627338eb0e43cfe410c0c1d15113f8cc5447dd5c76d6abca78e6e68eed28b76"
+    url "https://github.com/quarylabs/quary/releases/download/0.0.126/quary-0.0.126-quary-macos-x86_64-gnu.zip"
+    sha256 "7b9f8592f94a71704cb404a1457556efca5b2207fce122b2fb63db1ff63c6e78"
   elsif OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/quarylabs/quary/releases/download/0.0.116/quary-0.0.116-quary-macos-aarch64-gnu.zip"
-    sha256 "0c1bbb1413513c5168ad3e24a22cd6fcfa6a13f62ae3beac3114c914754882d7"
+    url "https://github.com/quarylabs/quary/releases/download/0.0.126/quary-0.0.126-quary-macos-aarch64-gnu.zip"
+    sha256 "72371d3913ba5743b72270e25ec8b463bfc2d20b54a6460276f3de94dcc94a1f"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/quarylabs/quary/releases/download/0.0.116/quary-0.0.116-quary-linux-x86_64-gnu.zip"
-    sha256 "d6adcc2578c0d4010b47ffa5714d333017c9768991487b99a1c88327a2322815"
+    url "https://github.com/quarylabs/quary/releases/download/0.0.126/quary-0.0.126-quary-linux-x86_64-gnu.zip"
+    sha256 "5da4015d8f22a502e253b905f39f1ce3b5f396da27286fc49c912c6949becbad"
   end
 
   def install
